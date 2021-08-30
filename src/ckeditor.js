bdabef3c6b39765f6973bcb7f55f031fe954ea8f
senkaPro <isenatpro@gmail.com>
2021-08-31 01:00:51 +0200

    Added default config

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -153,4 +153,171 @@ Editor.builtinPlugins = [
 	WordCount,
 ];
 
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'fontFamily',
+			'fontSize',
+			'fontColor',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'strikethrough',
+			'subscript',
+			'superscript',
+			'removeFormat',
+			'code',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'todoList',
+			'insertImage',
+			'strapiMediaLib',
+			'|',
+			'alignment',
+			'indent',
+			'outdent',
+			'|',
+			'specialCharacters',
+			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
+			'htmlEmbed',
+			'codeBlock',
+			'sourceEditing',
+			'horizontalLine',
+			'|',
+			'fullScreen',
+			'undo',
+			'redo',
+		],
+		shouldNotGroupWhenFull: true,
+	},
+	image: {
+		styles: ['alignLeft', 'alignCenter', 'alignRight'],
+		resizeOptions: [
+			{
+				name: 'resizeImage:original',
+				value: null,
+				icon: 'original',
+			},
+			{
+				name: 'resizeImage:50',
+				value: '50',
+				icon: 'medium',
+			},
+			{
+				name: 'resizeImage:75',
+				value: '75',
+				icon: 'large',
+			},
+		],
+		toolbar: [
+			'imageStyle:alignLeft',
+			'imageStyle:alignCenter',
+			'imageStyle:alignRight',
+			'|',
+			'imageTextAlternative',
+			'|',
+			'resizeImage:50',
+			'resizeImage:75',
+			'resizeImage:original',
+			'|',
+			'linkImage',
+		],
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells',
+			'tableProperties',
+			'tableCellProperties',
+		],
+	},
+	heading: {
+		options: [
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph',
+			},
+			{
+				model: 'heading1',
+				view: 'h1',
+				title: 'Heading 1',
+				class: 'ck-heading_heading1',
+			},
+			{
+				model: 'heading2',
+				view: 'h2',
+				title: 'Heading 2',
+				class: 'ck-heading_heading2',
+			},
+			{
+				model: 'heading3',
+				view: 'h3',
+				title: 'Heading 3',
+				class: 'ck-heading_heading3',
+			},
+			{
+				model: 'heading4',
+				view: 'h4',
+				title: 'Heading 4',
+				class: 'ck-heading_heading4',
+			},
+		],
+	},
+	htmlEmbed: {
+		showPreviews: true,
+		sanitizeHtml: inputHtml => {
+			const outputHtml = sanitizeHtml(inputHtml);
+			return {
+				html: outputHtml,
+				hasChanged: true,
+			};
+		},
+	},
+	fontFamily: {
+		options: [
+			'default',
+			'Arial, Helvetica, sans-serif',
+			'Courier New, Courier, monospace',
+			'Georgia, serif',
+			'Lucida Sans Unicode, Lucida Grande, sans-serif',
+			'Tahoma, Geneva, sans-serif',
+			'Times New Roman, Times, serif',
+			'Trebuchet MS, Helvetica, sans-serif',
+			'Verdana, Geneva, sans-serif',
+			'JetBrains Mono, monospace',
+			'Lato, Inter, sans-serif',
+		],
+	},
+	link: {
+		defaultProtocol: 'http://',
+		decorators: [
+			{
+				mode: 'manual',
+				label: 'Open in a new tab',
+				defaultValue: true,
+				attributes: {
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+			},
+			{
+				mode: 'manual',
+				label: 'Downloadable',
+				attributes: {
+					download: 'download',
+				},
+			},
+		],
+	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: 'en',
+};
 export default { Editor, EditorWatchdog };
